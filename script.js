0d96f95ec70fad827648555c60ef965a97b95874
titan11111 <extralife412@gmail.com>
2025-09-03 21:34:03 +0900

    Add rhythm game link

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // ゲームデータ
 const gameData = [
+    { title: 'リズムゲーム：ビートチャレンジ', category: 'action', keywords: 'リズム 音楽 ビート', url: 'https://titan11111.github.io/61-rizme/', icon: '🎵', isNew: true },
     { title: 'インベーダーブロック：宇宙の攻防', category: 'action', keywords: 'インベーダー ブロック シューティング パズル', url: 'https://titan11111.github.io/59-inveder-block/', icon: '👾', isNew: true },
     { title: 'インベーダー：宇宙防衛戦', category: 'action', keywords: 'インベーダー シューティング 宇宙', url: 'https://titan11111.github.io/57-inveder/', icon: '👾', isNew: true },
     { title: 'ブロック：積み上げパズル', category: 'action', keywords: 'ブロック パズル', url: 'https://titan11111.github.io/55-block/', icon: '🧱', isNew: true },
